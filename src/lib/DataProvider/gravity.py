570a5c36676e78f38def4ac5e71bcfb7c0ee4b57
jagio <jagio0129@gmail.com>
2018-10-01 23:48:35 +0900

    modify

--- src/lib/DataProvider/gravity.py
+++ src/lib/DataProvider/gravity.py
@@ -15,9 +15,11 @@ def dist_on_sphere(pos0, pos1, radious=earth_rad):
   xyz0, xyz1 = __latlng_to_xyz(*pos0), __latlng_to_xyz(*pos1)
   return acos(sum(x * y for x, y in zip(xyz0, xyz1)))*radious
 
-# グラビティモデルの式
+# グラビティモデルによる移動量の算出
 ## popA, popB = 人口、 distAB = AB間の距離
 def fomula(popA, popB, distAB):
   g = 1   # 何かしらのパラメータ
-  f = g * (popA * popB) / distAB
+  f = g * ((popA * popB) / distAB)
+
+  return f
 
