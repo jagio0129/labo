75ad917e6810b28c031c125605f68042a5ebb5eb
jagio <jagio0129@gmail.com>
2018-10-02 15:35:12 +0900

    マップの色味スケールをデータから動的に指定できるよう実装した

--- src/lib/Viewer/map.py
+++ src/lib/Viewer/map.py
@@ -30,9 +30,9 @@ def geojson_map(geojson_path, save_path):
 #       一番濃い色で塗られてしまい、データが正しく表現できない。
 def choropleth_map(geojson_path, show_data, save_path):
   noc_df = pd.read_csv(show_data)
-
+  max = noc_df.number.max()
+  
   m = init_map()
-
   m.choropleth(
     geo_data=geojson_path,
     name='choropleth',
@@ -41,13 +41,25 @@ def choropleth_map(geojson_path, show_data, save_path):
     line_opacity=0.2,
     key_on='feature.properties.N03_004',
     columns=['city', 'number'],
-    threshold_scale=[10, 30, 50, 70, 90, 110],
+    # threshold_scale=[0,30,60,90,120,150], # 最大６スケールまで
+    threshold_scale=calc_scale(max, 6), # 最大６スケールまで
     fill_color='PuBuGn',
     reset=True
   )
 
   folium.LayerControl().add_to(m)
   m.save(save_path)
+
+# コロプレスマップに表示するデータの色味のスケールの計算
+## return array
+def calc_scale(max_value, scale: int):
+  
+  v = int(max_value / scale)
+  ary = [0]
+  for num in range(1, scale, 1):
+    ary.append(v * num)
+  
+  return ary
 
 """
 # コロプレスマップを表示するHTMLファイルの作成。
