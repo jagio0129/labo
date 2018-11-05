e7553853a526d8ee738d8910b6e30760305c0822
jagio <jagio0129@gmail.com>
2018-11-05 21:41:24 +0900

    update

--- src/data_plot.py
+++ src/data_plot.py
@@ -21,8 +21,99 @@ GRAVITY_PATH = c["GRAVITY_PATH"]
 GRAVITY_IMAGE_PATH = GRAVITY_PATH + "/img"
 
 # 人口を１万で割る
-def optimize(pop):
+def divide_ten_thousond(pop):
   return round(pop / 10000, 3)
+
+# plot画像を生成する。
+def save_png(path, dir, tag, plt):
+  
+  # save path
+  date_name = utils.file_date(abs_file)
+  outpath = GRAVITY_IMAGE_PATH + "/" + dir + "/"+ tag + "/" + date_name + ".png"
+  file_path = os.path.dirname(outpath)
+  print("Create " + outpath)
+  
+  # ディレクトリが存在しなければ生成
+  if not os.path.exists(file_path):
+    os.makedirs(file_path)
+
+  plt.savefig(outpath)
+
+# x:距離、 y:移動量でプロット
+def plot(data_frame, path):
+  # データの用意
+  # origin_pop  = list(map(optimize, df["origin_pop"].values))
+  # dest_pop    = list(map(optimize, df["dest_pop"].values))
+  distance    = data_frame["distance"].values
+  amount      = data_frame['amount'].values
+  # r           = df['right_fomula'].values
+
+  # figure
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1)
+
+  # 散布図 config
+  ## sはドットの太さ。markerでドット文字を変更できる
+  ax.scatter(distance, amount, s=1)
+      
+  # general config
+  ax.set_title('Gravity Model')
+  ax.set_xlabel('DistanceAB')
+  ax.set_ylabel('Amount')
+
+  # save as png
+  save_png(path, "dist-amount-5", "default", plt)
+
+# 片対数計算で表示
+def single_log_plot(data_frame, path):
+  
+  distance    = data_frame["distance"].values
+  amount      = data_frame['amount'].values
+  
+  # figure
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1)
+
+  # 散布図 config
+  ## sはドットの太さ。markerでドット文字を変更できる
+  ax.scatter(distance, amount, s=1)
+      
+  # general config
+  ax.set_title('Gravity Model')
+  ax.set_xlabel('DistanceAB')
+  ax.set_ylabel('Amount')
+
+  # log
+  plt.xscale("log")
+
+  # save as png
+  save_png(path, "dist-amount", "singe-log", plt)
+
+# 両対数グラフ
+def multi_log_plot(data_frame, path):
+  
+  distance    = data_frame["distance"].values
+  amount      = data_frame['amount'].values
+  
+  # figure
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1)
+
+  # 散布図 config
+  ## sはドットの太さ。markerでドット文字を変更できる
+  ax.scatter(distance, amount, s=1)
+      
+  # general config
+  ax.set_title('Gravity Model')
+  ax.set_xlabel('DistanceAB')
+  ax.set_ylabel('Amount')
+
+  # log
+  plt.xscale("log")
+  plt.yscale("log")
+
+  # save as png
+  save_png(path, "dist-amount", "multi-log", plt)
 
 ### main
 if __name__ == '__main__':
@@ -35,33 +126,12 @@ if __name__ == '__main__':
 
     # csv load
     df = pd.read_csv(abs_file)
-    origin_pop  = list(map(optimize, df["origin_pop"].values))
-    dest_pop    = list(map(optimize, df["dest_pop"].values))
-    distance    = df["distance"].values
-    amount      = df['amount'].values
-    r           = df['right_fomula'].values
     
-    # figure
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    # 散布図 config
-    ## sはドットの太さ。markerでドット文字を変更できる
-    ax.scatter(amount, r, s=1)
-       
-    # general config
-    ax.set_title('Gravity Model')
-    ax.set_xlabel('Amount')
-    ax.set_ylabel('Right fomula')
-    plt.xlim([0,50])  # x range
-    plt.ylim([0,400])  # y range
-      
-    # save as png
-    date_name = utils.file_date(abs_file)
-    tags = "/x50y400"
-    outpath = GRAVITY_IMAGE_PATH + tags + "/figure-" + date_name + ".png"
-    print("Create " + outpath)
-    plt.savefig(outpath)
+    # plot 
+    plot(df, abs_file)
+    # single_log_plot(df, abs_file)
+    # multi_log_plot(df, abs_file)
 
   elapsed_time = time.time() - start
   print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
